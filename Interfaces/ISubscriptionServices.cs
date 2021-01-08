2a7b179d3a2943b85ea069312469a2de2053baf8
Cihat Yakar <cihat.yakar@nesine.com>
2021-01-08 09:20:06 +0300

    takım ve maçlar hazır..

--- Interfaces/ISubscriptionServices.cs
+++
@@ -1,9 +0,0 @@
-﻿using GraphQL.WebApi.Services;
-
-namespace GraphQL.WebApi.Interfaces
-{
-    public interface ISubscriptionServices
-    {
-        CityAddedService CityAddedService { get; }
-    }
-}
